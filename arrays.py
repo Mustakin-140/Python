460faae319c43ae9a01574123b3b54fbcb24fea1
Md Mustakin Mahi <63896978+Mustakin-140@users.noreply.github.com>
2022-10-21 20:38:50 +0600

    Add files via upload

---
+++ arrays.py
@@ -0,0 +1,15 @@
+language = ["C", "C++", "C#", "Python", "Java", "Ruby", "HTML"]
+print(language)
+
+x = len(language)
+
+print(x)
+
+y = language[3]
+print(y)
+
+language.append("JS")
+print(language)
+
+language.pop(2)
+print(language)
